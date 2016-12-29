72b2b856e1971be9015d50d25e96b188090b1091
IceFractal <jdenea@outlook.com>
2016-12-29 15:05:26 -0800

    Refactor server to use new index.html

--- app.js
+++ app.js
@@ -11,20 +11,20 @@ const users = require('./routes/users');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(serveStatic(path.join(__dirname, 'public')));
+// app.use(serveStatic(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(serveStatic(path.join(__dirname, 'client/dist'), {
+  index: 'index.html',
+}));
+
+// app.use('/', index);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
