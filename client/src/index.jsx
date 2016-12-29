7c3f55ce1777dba231aa6eb9719b762cd6c2b225
IceFractal <jdenea@outlook.com>
2016-12-29 15:18:33 -0800

    Update to use react

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,2 +1,8 @@
+import React    from 'react';
+import {render} from 'react-dom';
 
-console.log('Hello World');
+const App = () => (
+  <p>Hello World!</p>
+);
+
+render(<App/>, document.getElementById('app'));
