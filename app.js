2936b6ca06689e56e4e14a65b487eaa277a44186
IceFractal <jdenea@outlook.com>
2016-12-29 14:36:05 -0800

    Update boilerplate app to use es6

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-var express       = require('express');
-var path          = require('path');
-var favicon       = require('serve-favicon');
-var serveStatic   = require('serve-static');
-var logger        = require('morgan');
-var cookieParser  = require('cookie-parser');
-var bodyParser    = require('body-parser');
+const express       = require('express');
+const path          = require('path');
+const favicon       = require('serve-favicon');
+const serveStatic   = require('serve-static');
+const logger        = require('morgan');
+const cookieParser  = require('cookie-parser');
+const bodyParser    = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const index = require('./routes/index');
+const users = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -28,7 +28,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
