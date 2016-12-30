e02b5a95ab28f06bc016498e4bece8d8e7369e91
IceFractal <jdenea@outlook.com>
2016-12-30 10:48:36 -0800

    Add styling

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,15 @@
 import React      from 'react';
 import { render } from 'react-dom';
 
+const style = {
+  fontFamily: '"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif',
+  fontWeight: '300',
+  fontSize: '24px',
+  padding: '50px',
+};
+
 const App = () => (
-  <p>Hello World!</p>
+  <p style={style}>Hello World!</p>
 );
 
 render(<App/>, document.getElementById('app'));
