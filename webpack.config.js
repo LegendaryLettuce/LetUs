8bd599080921addb73d9a09ac31c5b1bee87994a
IceFractal <jdenea@outlook.com>
2016-12-30 00:58:47 -0800

    Add minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,25 @@
 const webpack = require('webpack');
 const path    = require('path');
 
+// const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
+// var plugins = [];
+// {
+//   plugins: [
+//     new FaviconsWebpackPlugin('my-logo.png'),
+//   ]
+// }
+
 const BUILD_DIR = path.resolve(__dirname, 'client/dist');
 const APP_DIR   = path.resolve(__dirname, 'client/src');
 
 const config = {
   entry: `${APP_DIR}/index.jsx`,
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+    }),
+  ],
   module: {
     loaders: [{
       test: /\.html$/,
