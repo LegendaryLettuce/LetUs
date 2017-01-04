3595a9e17056b3a17e89048a5dd84e6732654ff3
David An <davidank@users.noreply.github.com>
2017-01-04 11:23:34 -0800

    29 view create (#39)
    * Resolve merge conflict
    
    * Attempt to fix android graphical bug on tap
    
    * (view) Save changes to push and rebase

--- client/src/views/plan/create.jsx
+++ client/src/views/plan/create.jsx
@@ -3,28 +3,63 @@ import React, { Component }       from 'react';
 import { connect }      from 'react-redux';
 // Onsen UI
 import ons              from 'onsenui';
-import { Page, Dialog } from 'react-onsenui';
+import { Page, Toolbar, List, ListItem, Button } from 'react-onsenui';
 // Styles
 import styles           from '../../styles/styles';
 
-class Hello extends Component {
+class Create extends Component {
 
   constructor(props) {
     super(props);
-    ons.notification.alert(this.props.hello);
+  }
+
+  // Example Code from Docs for Row Generation
+  // renderRow(row, index) {
+  //   return (
+  //     <ListItem key={index}>
+  //       <div className='left'>
+  //         <img src={`http://placekitten.com/g/${x}/${y}`} className='list__item__thumbnail' />
+  //       </div>
+  //       <div className='center'>
+  //         {name}
+  //       </div>
+  //     </ListItem>
+  //   );
+  // }
+
+  renderRow(row, index) {
+    const x = 40 + Math.round(5 * (Math.random() - 0.5)),
+          y = 40 + Math.round(5 * (Math.random() - 0.5));
+
+    const names = ['Max', 'Chloe', 'Bella', 'Oliver', 'Tiger', 'Lucy', 'Shadow', 'Angel'];
+    const name = names[Math.floor(names.length * Math.random())];
+
+    return (
+      <ListItem key={index} modifier='longdivider' tappable>
+        <div className='left'>
+          <img src={`http://placekitten.com/g/${x}/${y}`} className='list__item__thumbnail' />
+        </div>
+        <label className='center'>
+          {name}
+        </label>
+      </ListItem>
+    );
+  }
+
+  // On Click Event
+  decideTogether() {
+    console.log('Deciding Together');
   }
 
   render() {
     return (
       <Page>
-        <Dialog
-          isOpen={true}
-        >
-          <div style={{ ...styles.body, color: 'white', textAlign: 'center', margin: '20px' }}>
-            <p style={{ opacity: 0.5 }}>{this.props.hello}</p>
-          </div>
-        </Dialog>
+        <List
+          dataSource={[1, 2, 3, 4, 5, 6, 7, 8]}
+          renderRow={this.renderRow}
+        />
       </Page>
+
     );
   }
 }
@@ -33,4 +68,15 @@ const mapStateToProps = state => ({
   hello: state.hello,
 });
 
-export default connect(mapStateToProps)(Hello);
+export default connect(mapStateToProps)(Create);
+
+        // <Toolbar>
+        //   <div className='center'>List</div>
+        // </Toolbar>
+// <Button 
+//           style={{ padding: '0px 40px 0px 40px' }}
+//           onClick={this.decideTogether}
+//           modifier='large'
+//         >Decide Together</Button>
+
+          // renderRow={(row, i) => <ListItem tappable tapBackgroundColor={'#d9d9d9'} key={i}><div className='center'>{row}</div></ListItem>}
