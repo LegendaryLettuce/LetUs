1531f7a547af20fb46dda02485fef2793b298a53
Autumn Smith <autumn07@gmail.com>
2017-01-05 12:30:29 -0800

    worked on database

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 // connect to letus with no username and password (null,null)
-const sequelize = new Sequelize('letus', null, null,
+sequelize = new Sequelize('letus', null, null,
   {
     dialect: 'postgres',
     port: 5432,
@@ -17,7 +17,7 @@ sequelize
 
 // schemas for DB
 // taken from EDS
-const User = sequelize.define('User', {
+exports.User = sequelize.define('User', {
   userid: { type: Sequelize.STRING, allowNull: false },
   pic: Sequelize.STRING,
   phonenumber: Sequelize.STRING,
@@ -25,29 +25,29 @@ const User = sequelize.define('User', {
   lettuceleaves: Sequelize.STRING,
 });
 
-const UserFavorites = sequelize.define('UserFavorites', {
+exports.UserFavorites = sequelize.define('UserFavorites', {
   userid: { type: Sequelize.STRING, allowNull: false },
   favoriteid: Sequelize.STRING,
 });
 
-const UserFriends = sequelize.define('UserFriends', {
+exports.UserFriends = sequelize.define('UserFriends', {
   userid: { type: Sequelize.STRING, allowNull: false },
   friendid: Sequelize.STRING,
 });
 
-const CheckIns = sequelize.define('CheckIns', {
+exports.CheckIns = sequelize.define('CheckIns', {
   attendee: Sequelize.STRING,
   eventid: Sequelize.STRING,
   checkedin: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
 });
 
-const Events = sequelize.define('Events', {
+exports.Events = sequelize.define('Events', {
   eventlord: { type: Sequelize.STRING, allowNull: false },
   yelpid: Sequelize.STRING,
   attendees: Sequelize.STRING,
 });
 
-const Favorites = sequelize.define('Favorites', {
+exports.Favorites = sequelize.define('Favorites', {
   yelpid: Sequelize.STRING,
 });
 
@@ -59,3 +59,16 @@ sequelize
   }, (err) => {
     console.log('An error occurred while creating the table:', err);
   });
+
+  var usertable = exports.User.build({
+    userid: 'john',
+  });
+  // Inserting Data into database
+  usertable.save().then(function(err) {
+    console.log('It worked');
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+
+  exports.User.sync();
--- db/userController.js
+++ db/userController.js
@@ -1,36 +1,26 @@
-// schemas for DB
-// taken from EDS
+var tables = require('./database.js');
 
-const User = sequelize.define('User', {
-  userid: { type: Sequelize.STRING, allowNull: false },
-  pic: Sequelize.STRING,
-  phonenumber: Sequelize.STRING,
-  friendrank: Sequelize.STRING,
-  lettuceleaves: Sequelize.STRING,
+var usertable = tables.User.build({
+  userid: 'john',
+});
+// Inserting Data into database
+usertable.save().then(function(err) {
+  console.log('It worked');
+})
+.catch(function(err) {
+  console.log(err);
 });
 
-const UserFavorites = sequelize.define('UserFavorites', {
-  userid: { type: Sequelize.STRING, allowNull: false },
-  favoriteid: Sequelize.STRING,
-});
+tables.User.sync();
 
-const UserFriends = sequelize.define('UserFriends', {
-  userid: { type: Sequelize.STRING, allowNull: false },
-  friendid: Sequelize.STRING,
-});
 
-const CheckIns = sequelize.define('CheckIns', {
-  attendee: Sequelize.STRING,
-  eventid: Sequelize.STRING,
-  checkedin: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: true },
-});
-
-const Events = sequelize.define('Events', {
-  eventlord: { type: Sequelize.STRING, allowNull: false },
-  yelpid: Sequelize.STRING,
-  attendees: Sequelize.STRING,
-});
-
-const Favorites = sequelize.define('Favorites', {
-  yelpid: Sequelize.STRING,
-});
+// tables.User
+//   .find({ where: { userid: 'john' } })
+//   .then(function(err, johnDoe) {
+//     if (!johnDoe) {
+//       console.log('No user with the username "john-doe" has been found.');
+//     } else {
+//       console.log('Hello ' + johnDoe.username + '!');
+//       console.log('All attributes of john:', johnDoe.get());
+//     }
+//   });
