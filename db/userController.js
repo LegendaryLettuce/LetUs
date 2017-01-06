3005a34d73885b72f01545305fe6bcd25d59dc82
Autumn Smith <autumn07@gmail.com>
2017-01-05 16:10:45 -0800

    inserting into database test

--- db/userController.js
+++ db/userController.js
@@ -1,1 +1,19 @@
-const omar = 'dont trust the squash';
+const { User, UserInfo, Friends, CheckIns, Events, Favorites } = require('./userSchema.js');
+
+// inserting for now, middleware later
+
+const newUser = new User({
+  userid: 'john doe',
+  pic: 'https://www.occrp.org/images/stories/CCWatch/16147_2.jpg',
+  phonenumber: '555-555-5555',
+  friendrank: 1,
+  lettuceleaves: 0,
+});
+
+newUser.save((err, success) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Inserted');
+  }
+});
