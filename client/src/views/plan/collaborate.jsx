849b2211496ee5a39334a2e54dee484e50af039d
IceFractal <jdenea@outlook.com>
2017-01-06 20:19:43 -0800

    (feat) Major style update to cards

--- client/src/views/plan/collaborate.jsx
+++ client/src/views/plan/collaborate.jsx
@@ -7,6 +7,7 @@ import { Page, Carousel, CarouselItem } from 'react-onsenui';
 // import styles           from '../../styles/styles';
 
 const [RED, GREEN, BLUE] = [0, 1, 2];
+const WIDTH_PERCENT = 66;
 
 class Collaborate extends Component {
 
@@ -22,7 +23,7 @@ class Collaborate extends Component {
     this.index = 0;
     this.rgb = [200, 200, 200]; // [181, 198, 208];
     this.otherRGB = 200;
-    this.rgbMin = 100;
+    this.rgbMin = 30;
     this.rgbMax = 200;
     this.x = window.innerWidth / 2;
     this.diffx = 0;
@@ -83,8 +84,11 @@ intensity - ${Math.floor(((this.rgbMax - this.otherRGB) / (this.rgbMax - this.rg
         this.setState({
           word: this.props.yelpData[this.index].displayTitle,
           rgb: this.rgb,
-          pos: 1,
-          anim: {},
+        }, () => {
+          setTimeout(this.setState.bind(this, {
+            pos: 1,
+            anim: {},
+          }), 30);
         });
       });
     }
@@ -196,95 +200,117 @@ intensity - ${Math.floor(((this.rgbMax - this.otherRGB) / (this.rgbMax - this.rg
         onMouseMove={this.moveMouse.bind(this)}
         onTouchMove={this.moveTouch.bind(this)}
       >
-        <Carousel
-          fullscreen
-          swipeable
-          autoScroll
-          overscrollable
-          index={this.state.pos}
-          onPostChange={this.onFlick.bind(this)}
-          animationOptions={this.state.anim}
-          onTouchStart={this.onHoldStart.bind(this)}
-          onTouchEnd={this.onHoldEnd.bind(this)}
-          onMouseDown={this.onHoldStart.bind(this)}
-          onMouseUp={this.onHoldEnd.bind(this)}
-          style={{
-            zIndex: '3',
-            // background: '#242424',
-            // WebkitBackgroundClip: '',
-            // backgroundClip: '',
-          }}
-        >
-          <CarouselItem/>
-          <CarouselItem>
-            <div style={{
-              height: `${50 - this.calculateHeight(2)}%`,
-            }}/>
-            <div style={{
-              height: `${this.calculateHeight()}%`,
-              width: '66%',
-              marginLeft: '16%',
-              borderRadius: '10%',
-              boxShadow: '0 0 0 10000em #242424',
-              border: '1px solid #ccc',
-              overflow: 'hidden',
-              // zIndex: '3',
-              // border: '1px solid #ccc',
-            }}>
+          <Carousel
+            fullscreen
+            swipeable
+            autoScroll
+            overscrollable
+            index={this.state.pos}
+            onPostChange={this.onFlick.bind(this)}
+            animationOptions={this.state.anim}
+            onTouchStart={this.onHoldStart.bind(this)}
+            onTouchEnd={this.onHoldEnd.bind(this)}
+            onMouseDown={this.onHoldStart.bind(this)}
+            onMouseUp={this.onHoldEnd.bind(this)}
+            style={{
+              zIndex: '3',
+              // background: '#242424',
+              // WebkitBackgroundClip: '',
+              // backgroundClip: '',
+            }}
+          >
+            <CarouselItem/>
+            <CarouselItem>
               <div style={{
-                height: '100%',
-                widht: '100%',
-                // height: `${this.calculateHeight()}%`,
-                // width: '66%',
-                // marginLeft: '16%',
-                fontSize: 'xx-large',
-                textAlign: 'center',
-                background: `rgba(${this.state.rgb[0]},${this.state.rgb[1]},${this.state.rgb[2]},0.6)`,
-                color: '#242424', // TODO: get color from OnsenUI
-                borderRadius: '10%',
-                boxShadow: '0 0 1em 0 #333 inset',
+                height: `${50 - this.calculateHeight(2)}%`,
+              }}/>
+              <div style={{
+                height: `${this.calculateHeight() + 3}%`, // TODO: change size based on text lines
+                width: `${WIDTH_PERCENT}%`,
+                marginLeft: `${((100 - WIDTH_PERCENT) / 2)}%`,
+                borderRadius: `${0.1 * this.state.windowWidth}px`,
+                boxShadow: '0 0 0 10000em #242424',
+                border: `1px solid rgb(${this.state.rgb[0]},${this.state.rgb[1]},${this.state.rgb[2]})`, // #ccc
+                overflow: 'hidden',
+                // zIndex: '3',
                 // border: '1px solid #ccc',
               }}>
                 <div style={{
-                  // fontWeight: '900',
-                  // WebkitTextStroke: '1px #ccc',
-                  // textShadow: '0 0 1em #000', // , 0 0 .33em rgba(0,0,0,.33)
-                  padding: '10%',
-                  paddingTop: '70%',
+                  height: '100%',
+                  width: '100%',
+                  // height: `${this.calculateHeight()}%`,
+                  // width: `${WIDTH_PERCENT}%`,
+                  // marginLeft: `${((100 - WIDTH_PERCENT) / 2)}%`,
+                  fontSize: 'xx-large',
+                  textAlign: 'center',
+                  background: `rgba(${this.state.rgb[0]},${this.state.rgb[1]},${this.state.rgb[2]},0)`,
+                  // color: '#242424', // TODO: get color from OnsenUI
+                  borderRadius: '10%',
+                  boxShadow: '0 0 2em 0 #333 inset',
+                  // border: '1px solid #ccc',
                 }}>
-                  {'' /* this.state.word */}
+                  <div style={{
+                    // fontWeight: '900',
+                    // WebkitTextStroke: '1px #ccc',
+                    // textShadow: '0 0 1em #000', // , 0 0 .33em rgba(0,0,0,.33)
+                    padding: '10%',
+                    paddingTop: '70%',
+                  }}>
+                    {'' /* this.state.word */}
+                  </div>
                 </div>
               </div>
-            </div>
-          </CarouselItem>
-          <CarouselItem/>
-        </Carousel>
-        <div style={{
-          height: `${50 - this.calculateHeight(2)}%`,
-        }}/>
-        <div style={{
-          fontSize: 'xx-large',
-          textAlign: 'center',
-          // color: '#242424', // TODO: get color from OnsenUI
-          position: 'fixed',
-          height: `${this.calculateHeight()}%`,
-          width: '66%',
-          marginLeft: '16%',
-          zIndex: '2',
-        }}>
+            </CarouselItem>
+            <CarouselItem/>
+          </Carousel>
           <div style={{
-            padding: '10%',
-            paddingTop: '70%',
-            // TODO: place x and o icons on left and right hidden until revealed by card
-            // TODO: make icon colors more saturated if more intense
-            // TODO: replace background change with loading bar
-            // OR
-            // TODO: attach circle to window and make x or check appear on swipe
+            height: `${50 - this.calculateHeight(2)}%`,
+          }}/>
+          <div style={{
+            fontSize: 'xx-large',
+            textAlign: 'center',
+            color: '#242424', // TODO: get color from OnsenUI
+            position: 'fixed',
+            height: `${this.calculateHeight() + 3.1}%`,
+            width: `${WIDTH_PERCENT + 0.1}%`,
+            marginLeft: `${((100 - WIDTH_PERCENT) / 2)}%`,
+            zIndex: '2',
+            borderRadius: `${0.1 * this.state.windowWidth}px`,
+            boxShadow: '0 0 0 10000em #ccc',
+            background: '#888',
           }}>
-            <image src={this.getUrl()}/>
-            {this.state.word}
+            <div style={{
+              // color: '#242424',
+              // padding: '10%',
+              // paddingTop: '70%',
+              // TODO: place x and o icons on left and right hidden until revealed by card
+              // TODO: make icon colors more saturated if more intense
+              // TODO: replace background change with loading bar
+              // OR
+              // TODO: attach circle to window and make x or check appear on swipe
+            }}>
+              <div
+                style={{
+                  height: `${(((this.calculateHeight() - 10) / 100) * this.state.windowHeight) + 1}px`,
+                  width: `${((WIDTH_PERCENT / 100) * this.state.windowWidth) + 1}px`,
+                  // boxShadow: '0 0 0 1px #242424',
+                  borderTopRightRadius: `${0.1 * this.state.windowWidth}px`,
+                  borderTopLeftRadius: `${0.1 * this.state.windowWidth}px`,
+                  overflow: 'hidden',
+                }}
+              >
+                <img
+                  src={this.getUrl(this.props.yelpData[this.index].imageUrl)}
+                  alt=''
+                  height={`${(this.calculateHeight() / 100) * this.state.windowHeight}`}
+                  width={`${(WIDTH_PERCENT / 100) * this.state.windowWidth}`}
+                />
+              </div>
+              <div style={{ padding: `${0.01 * this.state.windowHeight}px` }}>
+                {this.state.word}
+              </div>
+            </div>
           </div>
-        </div>
       </Page>
     );
   }
