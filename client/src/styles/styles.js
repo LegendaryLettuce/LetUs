e5e6aeb140729a851169da63fd48f4aa81d7fb6f
Autumn Smith <autumn07@gmail.com>
2017-01-06 16:13:30 -0800

    added opacity to background image

--- client/src/styles/styles.js
+++ client/src/styles/styles.js
@@ -14,14 +14,13 @@ export const bottomNavStyle = {
 };
 
 export const login = {
-  'background-image': 'url("http://68.media.tumblr.com/233a17b2322253404dfc6ce97501613b/tumblr_oh350r0AtK1u9ooogo1_540.gif")',
+  'background-image': 'url("http://68.media.tumblr.com/b4f33d654b4448794313633770cc8572/tumblr_oh34v07h9w1u9ooogo1_540.gif")',
   position: 'fixed',
   width: '100%',
   height: '100vh',
 };
 
 export const fbLogin = {
-  'font-size': 'xx-large',
   background: 'none',
   'margin-bottom': '30px',
   display: 'flex',
@@ -30,13 +29,36 @@ export const fbLogin = {
 };
 
 export const splashText = {
-  'margin-top': '250px',
+  color: 'white',
+  'margin-top': '150px',
+  'margin-bottom': '100px',
   fontFamily: '"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif',
   fontWeight: '300',
-  fontSize: '75px',
+  fontSize: '90px',
   'text-align': 'center',
+  'text-shadow': '0px 0px 3px #000',
 };
 
+export const tint = {
+  content: '""',
+  position: 'absolute',
+  top: '0',
+  left: '0',
+  right: '0',
+  bottom: '0',
+  width: '100%',
+  height: '100%',
+  'background-color': 'rgba(0,0,0,.5)',
+  margin: 'auto',
+};
+
+export const tagline = {
+  'text-align': 'center',
+  fontFamily: '"HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif',
+  fontWeight: '300',
+  fontSize: '24px',
+  color: 'white',
+};
 
 // export const titleStyle = {
 //   ...styles.body,
--- client/src/views/login/login.jsx
+++ client/src/views/login/login.jsx
@@ -5,7 +5,7 @@ import { connect }          from 'react-redux';
 import ons                  from 'onsenui';
 import { Icon, Button, BottomToolbar }             from 'react-onsenui';
 // Styles
-import { login, splashText, fbLogin, bodyStyle } from '../../styles/styles';
+import { login, splashText, fbLogin, bodyStyle, tint, tagline } from '../../styles/styles';
 import TextCarousel from 'react-text-carousel';
 
 // const linkStyle = {
@@ -31,18 +31,22 @@ const button = {
   'font-size': 'x-large',
 };
 
-const phrases = ["eat.", "drink.", "play.", "send nudes."]; // Required
+const phrases = [' eat.', ' drink.', ' play.']; // Required
 const interval = 2000; // The time to wait before rendering the next string
 const typistProps = {} // Props that are passed to the react-typist component
 
 const Login = () => (
   <div style={login}>
-    wtf
-    <div style={splashText}>Let Us <TextCarousel phrases={phrases} interval={interval} typistProps={typistProps} /></div>
-    <div style={bodyStyle}>Collaborate event planning with friends.</div>
-    <BottomToolbar style={fbLogin}>
-      <Button style={button}><Icon icon="fa-facebook-square"/> Login with Facebook</Button>
-    </BottomToolbar>
+    <div style={tint}>
+      <div style={splashText}>
+        Let Us
+        <TextCarousel phrases={phrases} interval={interval} typistProps={typistProps} />
+      </div>
+      <div style={tagline}>Collaborate event planning with your friends.</div>
+      <BottomToolbar style={fbLogin}>
+        <Button style={button}><Icon icon="fa-facebook-square"/> Login with Facebook</Button>
+      </BottomToolbar>
+    </div>
   </div>
 
 );
