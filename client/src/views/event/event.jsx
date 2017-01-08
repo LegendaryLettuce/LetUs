149beeab3b57dfb14937460dcb5531f458107ec4
Autumn Smith <autumn07@gmail.com>
2017-01-07 17:20:09 -0800

    add bars to event view

--- client/src/views/event/event.jsx
+++ client/src/views/event/event.jsx
@@ -3,17 +3,19 @@ import React, { Component }     from 'react';
 import { connect }              from 'react-redux';
 // Onsen UI
 import ons                      from 'onsenui';
-import { Page, List, ListItem, Icon, Dialog } from 'react-onsenui';
+import { Page, List, ListItem, Icon, Toolbar } from 'react-onsenui';
 // Styles
-import { bodyStyle, Tint }            from '../../styles/styles';
+import { bodyStyle }            from '../../styles/styles';
+
+import  BottomNav        from './../../views/_global/bottomNav.jsx';
 
 const dummyData = {
   rating: 4.5,
-  'snippet_text': 'This place does Korean - Mexican fusion so well! The prices are also super fair for San Francisco. \n\nMy friends and I were about to get two The Big...',
+  snippet_text: 'This place does Korean - Mexican fusion so well! The prices are also super fair for San Francisco. \n\nMy friends and I were about to get two The Big...',
   mobile_url: 'https://m.yelp.com/biz/tacorea-san-francisco?adjust_creative=e6iE0U7lMHSSLdMk1-vrXw&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=e6iE0U7lMHSSLdMk1-vrXw',
   rating_img_url: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/99493c12711e/ico/stars/v1/stars_4_half.png',
   name: 'Tacorea',
-  'image_url': 'https://s3-media1.fl.yelpcdn.com/bphoto/7sOn8_P7Hxb0ZkM55tz3mg/l.jpg',
+  image_url: 'https://s3-media1.fl.yelpcdn.com/bphoto/7sOn8_P7Hxb0ZkM55tz3mg/l.jpg',
   rating_img_url_small: 'https://s3-media2.fl.yelpcdn.com/assets/2/www/img/a5221e66bc70/ico/stars/v1/stars_small_4_half.png',
   url: 'https://www.yelp.com/biz/tacorea-san-francisco?adjust_creative=e6iE0U7lMHSSLdMk1-vrXw&utm_campaign=yelp_api&utm_medium=api_v2_search&utm_source=e6iE0U7lMHSSLdMk1-vrXw',
   phone: '4158851325',
@@ -24,9 +26,23 @@ const dummyData = {
     display_address: [
       '809 Bush St',
       'Union Square',
-      'San Francisco, CA 94108'
+      'San Francisco, CA 94108',
     ],
   },
+  categories: [
+    [
+      'Mexican',
+      'mexican',
+    ],
+    [
+      'Korean',
+      'korean',
+    ],
+    [
+      'Latin American',
+      'latin',
+    ],
+  ],
 };
 
 const iconPadding = {
@@ -36,7 +52,7 @@ const iconPadding = {
 const imageDiv = {
   ...bodyStyle,
   background: `url(${dummyData.image_url})`,
-  height: '250px',
+  height: '300px',
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   backgroundPosition: 0,
@@ -58,7 +74,7 @@ const tagline = {
 };
 
 const imageTint = {
-  height: '250px',
+  height: '300px',
   backgroundColor: 'rgba(0,0,0,.5)',
   margin: 'auto',
   position: 'relative',
@@ -69,6 +85,14 @@ const titleText = {
   position: 'absolute',
   bottom: 0,
   textAlign: 'center',
+};
+
+const tags = {
+  'border-radius': '12px',
+  'padding': '5px',
+  textAlign: 'center',
+  background: 'gray',
+  margin: '7px',
 };
 
 
@@ -89,6 +113,13 @@ class Event extends Component {
     return a;
   }
 
+  renderToolbar(toolbarTitle) {
+    return (
+      <Toolbar>
+        <div className='center' style={{ fontWeight: 'bolder' }}>{toolbarTitle}</div>
+      </Toolbar>
+    );
+  }
 
   // router
   // decideCreate() {
@@ -97,7 +128,8 @@ class Event extends Component {
 
   render() {
     return (
-        <Page modifier='shop-details'>
+        <Page modifier='shop-details' renderToolbar={() => this.renderToolbar('Event Details')}
+        style={{ background: 'rgba(51,51,51,1)' }}>
           <div style={imageDiv}>
             <div style={imageTint}>
               <div style={titleText}>
@@ -111,11 +143,28 @@ class Event extends Component {
               </div>
             </div>
           </div>
+          <div style={{ display: 'flex', 'justify-content': 'center' }}>
+            {dummyData.categories.map((cat, i) => (
+              <div style={tags} key={i}>{cat[0]}</div>
+            ))}
+          </div>
           <List>
-            <ListItem tappable><span><Icon icon='fa-map-marker' style={iconPadding}/></span><span>{dummyData.location.display_address.join(', ')}</span></ListItem>
-            <ListItem tappable><Icon icon='fa-phone' style={iconPadding} />{dummyData.display_phone}</ListItem>
-            <ListItem tappable><Icon icon='fa-phone' style={iconPadding} />Yelp Page</ListItem>
+            <a href={`http://maps.google.com/?q=${dummyData.location.display_address.join(', ')}`} target='_blank' style={{ textDecoration: 'none' }}>
+              <ListItem tappable>
+                <Icon icon='fa-map-marker' style={iconPadding}/>
+                {dummyData.location.display_address.join(', ')}
+              </ListItem>
+            </a>
+            <ListItem tappable><Icon icon='fa-phone' style={iconPadding} />
+              {dummyData.display_phone}
+            </ListItem>
+            <a href={dummyData.mobile_url} target='_blank' style={{ textDecoration: 'none' }}>
+              <ListItem tappable>
+                <Icon icon='fa-yelp' style={iconPadding} />Visit {dummyData.name} on Yelp!
+              </ListItem>
+            </a>
           </List>
+          <BottomNav></BottomNav>
         </Page>
     );
   }
