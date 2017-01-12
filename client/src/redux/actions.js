1e070c2fc3ab9fab6201ee413a99f01844060857
IceFractal <jdenea@outlook.com>
2017-01-11 16:33:05 -0800

    (feat) Add session storage

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,14 +1,16 @@
 
 export const updateInviteFriends = friends => ({ type: 'UPDATE_INVITE_FRIENDS', friends });
 
+export const updateEventHash = eventHash => ({ type: 'UPDATE_EVENT_HASH', eventHash });
+
 export const updateYelpData = yelpData => ({ type: 'UPDATE_YELP_DATA', yelpData });
 
 export const addLiveData = element => ({ type: 'ADD_LIVE_DATA', element });
 
-export const updateEventHash = eventHash => ({ type: 'UPDATE_EVENT_HASH', eventHash });
-
-export const updateUser = token => ({ type: 'UPDATE_USER', token });
+export const updateUser = user => ({ type: 'UPDATE_USER', user });
 
 export const updateConnectedPeers = connectedPeers => ({ type: 'UPDATED_CONNECTED_PEERS', connectedPeers });
 
 export const updateTalliedVotes = talliedVotes => ({ type: 'UPDATED_TALLIED_VOTES', talliedVotes });
+
+export const load = state => ({ type: 'LOAD', state });
--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -5,6 +5,15 @@ export const postLogin = user => (
   axios.post('/login', user)
 );
 
+export const getStore = () => ({
+  friends: sessionStorage.getItem('friends') || [],
+  yelpData: sessionStorage.getItem('yelpData') || [],
+  eventHash: sessionStorage.getItem('eventHash') || [],
+  element: sessionStorage.getItem('element') || '',
+  user: sessionStorage.getItem('user') || null,
+  loaded: true,
+});
+
 // export const post = data => (
 //   new Promise((resolve, reject) => {
 //     resolve(data);
