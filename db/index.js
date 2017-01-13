f0c082e1030f5d35285d58ad2db16b09aa4b8656
wilsonhyng <wilsonhyng@gmail.com>
2017-01-13 12:37:49 -0800

    (feat) docker img/containers for server/webpack/db

--- db/index.js
+++ db/index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/letus';
+// const mongoUri = 'mongodb://localhost/letus';
+const mongoUri = 'mongodb://database/docker';
+
+
 mongoose.connect(mongoUri);
 
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
