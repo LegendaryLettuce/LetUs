b18c2d06f96a96124ec5316b5e573050f76c09ba
IceFractal <jdenea@outlook.com>
2017-01-12 16:12:45 -0800

    (cleanup) Make topBar and bottomButton global elements

---
+++ client/src/views/_global/bottomButton.jsx
@@ -0,0 +1,18 @@
+import React            from 'react';
+// Onsen UI
+import { Button }       from 'react-onsenui';
+// Styles
+import { buttonStyle }  from '../../styles/styles';
+
+/**
+ * Creates a button used for routing that is placed above the bottomNav
+ * @param {object} props - React props
+ * @param {string} props.title - Text to be displayed
+ * @param {requestCallback} props.routeHandler - Callback to route to the next page
+ * @extends {React.Component}
+ */
+const BottomButton = ({ title, routeHandler }) => (
+  <Button style={buttonStyle} modifier='large' onClick={routeHandler}>{title}</Button>
+);
+
+export default BottomButton;
---
+++ client/src/views/_global/topBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+// Onsen UI
+import { Toolbar, BackButton } from 'react-onsenui';
+
+/**
+ * Place into Onsen Page renderToolbar Attribute
+ * @param {Object} props - React props
+ * @param {string} props.title - Text to be displayed
+ * @param {requestCallback} props.handleBack - Callback to route to the previous page
+ * @extends {React.Component}
+ */
+const TopBar = ({ title, handleBack }) => (
+  <Toolbar>
+    <div className="left">
+      <BackButton onClick={handleBack}></BackButton>
+    </div>
+    <div className='center' style={{ fontWeight: 'bolder' }}>{title}</div>
+  </Toolbar>
+);
+
+export default TopBar;
