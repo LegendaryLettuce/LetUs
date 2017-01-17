306374a87a27eb0d0ac1e632eb13d76fb365ee43
wilsonhyng <wilsonhyng@gmail.com>
2017-01-16 23:29:52 -0800

    (refactor) change Home events to stateful

--- client/src/views/home/components/events.jsx
+++ client/src/views/home/components/events.jsx
@@ -1,32 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
+// Redux
+import { connect }          from 'react-redux';
 // Onsen UI
 import { ListItem, List, Icon } from 'react-onsenui';
 // Styles
 // import { }              from '../../../styles/styles';
 
-const Events = ({ events }) => (
-  <List
-    dataSource={events}
-    renderRow={
-      (event, idx) => (
-        <ListItem key={idx} modifier={idx === events.length - 1 ? 'longdivider' : null}>
-        <div>
-          {console.log(JSON.parse(event.topEvent))}
-        </div>
-          <div className="left">
-            <img className="list__item__image" src={JSON.parse(event.topEvent)[0].imageUrl} />
-          </div>
-          <div className="center">
-            <span className="list__item__title">{JSON.parse(event.topEvent)[0].displayTitle}</span>
-            <span className="list__item__subtitle">{JSON.parse(event.topEvent)[0].snippetText}</span>
-          </div>
+class Events extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-        </ListItem>
-      )
-    }
-  />
-);
+  render() {
+    return (
+      <List
+        dataSource={this.props.events}
+        renderRow={
+          (event, idx) => (
+            <ListItem key={idx} modifier={idx === this.props.events.length - 1 ? 'longdivider' : null} onClick={() => {
+              this.props.handleTouch(event);
+              console.log(event);
+            }}>
+              <div className="left">
+                <img className="list__item__image" src={JSON.parse(event.topEvent)[0].imageUrl} />
+              </div>
+              <div className="center">
+                <span className="list__item__title">{JSON.parse(event.topEvent)[0].displayTitle}</span>
+                <span className="list__item__attendees">
+                  Attendees <br />
+                  {JSON.parse(event.attendees).reduce((memo, attendee) => memo + ' ' + attendee.name, '')}
+                </span>
+              </div>
+            </ListItem>
+          )
+        }
+      />
+    );
+  }
+}
 
-export default Events;
+const mapStateToProps = state => ({
 
-            // console.log(JSON.parse(event.topEvent)[idx].imageUrl)
+});
+
+export default connect(mapStateToProps)(Events);
+
+
+// const Events = ({ handleTouch, events }) => (
+//   <List
+//     dataSource={events}
+//     renderRow={
+//       (event, idx) => (
+//         <ListItem key={idx} modifier={idx === events.length - 1 ? 'longdivider' : null} onClick={() => {
+//           handleTouch(event);
+//           console.log(event);
+//         }}>
+//           <div className="left">
+//             <img className="list__item__image" src={JSON.parse(event.topEvent)[0].imageUrl} />
+//           </div>
+//           <div className="center">
+//             <span className="list__item__title">{JSON.parse(event.topEvent)[0].displayTitle}</span>
+//             <span className="list__item__attendees">
+//               Attendees <br />
+//               {JSON.parse(event.attendees).reduce((memo, attendee) => memo + ' ' + attendee.name, '')}
+//             </span>
+//           </div>
+//         </ListItem>
+//       )
+//     }
+//   />
+// );
+
+// export default Events;
+
+            // <span className="list__item__subtitle">{JSON.parse(event.topEvent)[0].snippetText}</span>
+            // {JSON.parse(event.attendees)[0].name}
