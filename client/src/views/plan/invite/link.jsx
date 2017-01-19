1f2b325f29cb968c36a5f3bbb4c1d06cae4eefee
davidank <dandahmann@gmail.com>
2017-01-18 17:57:01 -0800

    (feat) init copy feedback on invite page

--- client/src/views/plan/invite/link.jsx
+++ client/src/views/plan/invite/link.jsx
@@ -1,4 +1,4 @@
-import React            from 'react';
+import React, { Component } from 'react';
 // Redux
 import { connect }      from 'react-redux';
 // Copy Button
@@ -11,7 +11,7 @@ import { Icon }         from 'react-onsenui';
 const linkStyle = {
   border: '2px solid white',
   borderRadius: '10px',
-  width: '80%',
+  width: '65%',
   height: '3.5%',
   margin: 'auto',
   padding: '10px',
@@ -19,6 +19,7 @@ const linkStyle = {
   textAlign: 'center',
   marginBottom: '5%',
 };
+
 const copyStyle = {
   right: '0',
   margin: '0',
@@ -33,22 +34,73 @@ const hostUrl = `${window.location.href}`;
 
 const concatHostUrl = hostUrl.split('/')[2];
 
-const Link = props => (
-  <div style={linkStyle}>
-    <div style={{ float: 'left' }}>
-      <p style={link}>{`${concatHostUrl}/c/${props.eventHash}`}</p>
-    </div>
-    <CopyToClipboard
-      text={`${hostUrl}c/${props.eventHash}`}
-      onCopy={() => {
-        console.log('COPIED:', `${hostUrl}c/${props.eventHash}`);
-      }}
-    >
-      <Icon icon='fa-clone' style={copyStyle}/>
-    </CopyToClipboard>&nbsp;
-  </div>
+class Link extends Component {
+  constructor(props) {
+    super(props);
 
-);
+    this.animationDur = 1;
+
+    this.state = {
+      copied: false,
+      transparency: 1,
+    };
+    this.copyFeedback = this.copyFeedback.bind(this);
+    this.animateTransparency = this.animateTransparency.bind(this);
+  }
+
+  animateTransparency() {
+    if (this.state.copied && (this.state.transparency >= 0)) {
+      console.log('ANIMATING TRANSP');
+      this.setState({
+        transparency: this.state.transparency - ((1 / 60) * (1 / this.animationDur)),
+      });
+      setTimeout(() => {
+        this.animateTransparency();
+      }, (1000 / 60));
+    }
+  }
+
+  copyFeedback() {
+    console.log('COPY FEEDBACK EXECUTED');
+    if (!this.state.copied) {
+      this.setState({
+        copied: true,
+      }, () => {
+        this.animateTransparency();
+        setTimeout(() => {
+          this.setState({
+            copied: false,
+            transparency: 1,
+          });
+        }, (this.animationDur * 1000));
+      });
+    }
+  }
+
+  render() {
+    const linkStyleCopied = {
+      ...linkStyle,
+      backgroundColor: `rgba(255, 255, 255, ${this.state.transparency})`,
+    };
+
+    return (
+      <CopyToClipboard
+        text={`${hostUrl}c/${this.props.eventHash}`}
+        onCopy={() => {
+          console.log('COPIED:', `${hostUrl}c/${this.props.eventHash}`);
+          this.copyFeedback();
+        }}
+      >
+        <div style={this.state.copied ? linkStyleCopied : linkStyle}>
+          <div style={{ float: 'left' }}>
+            <p style={link}>{`${concatHostUrl}/c/${this.props.eventHash}`}</p>
+          </div>
+          <Icon icon='fa-clone' style={copyStyle}/>
+        </div>
+      </CopyToClipboard>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   eventHash: state.eventHash,
