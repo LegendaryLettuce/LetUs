c21ff8432384fbcc7adafe4ee3fc2bf21757b475
wilsonhyng <wilsonhyng@gmail.com>
2017-01-19 18:45:41 -0800

    (clean up) delete unused files

--- db-old/db/models/checkins.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('CheckIns', {
-    attendee: DataTypes.STRING,
-    eventid: DataTypes.STRING,
-    checkedin: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: true },
-  });
-};
--- db-old/db/models/userfriends.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (sequelize, DataTypes) {
-  return sequelize.define('UserFriends', {
-    userid: { type: DataTypes.STRING, allowNull: false },
-    friendid: DataTypes.STRING,
-  });
-};
